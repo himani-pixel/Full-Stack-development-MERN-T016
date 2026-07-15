07fc55822c5cb8c89e088ececd3b357abca36719
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:36:27 +0530

    Create Practical_1D.jsx

---
+++ Practical 1/Practical_1D.jsx
@@ -0,0 +1,20 @@
+let num1 = 15;
+let num2 = 25;
+
+
+function add(a , b){
+   return a + b;
+}
+
+
+function multiply(a , b){
+   return a * b;
+}
+
+
+console.log("1st number: ", num1);
+console.log("2nd number: ",num2);
+
+
+console.log("Addition = ", add(num1 , num2));
+console.log("Multiplication = ", multiply(num1 , num2))
