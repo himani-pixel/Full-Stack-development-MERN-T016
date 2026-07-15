a545ae54695865d7d411a5d96acbeed40af1b3c6
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 18:12:33 +0530

    Add files via upload

---
+++ Practical 4/Practical_4A/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 M. V. L. U. Placement Cell</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Practical 4/Practical_4A/PlacementStatus.jsx
@@ -0,0 +1,12 @@
+function PlacementStatus(){
+    return(
+        <section className="section">
+            <h2>Placement Statistics</h2>
+            <p>Total Students: 180</p>
+            <p>Placed Students: 142</p>
+            <p>Remaining Students: 38</p>
+            <p>Placement Percentage: 78.89%</p>
+        </section>
+    );
+}
+export default PlacementStatus;
