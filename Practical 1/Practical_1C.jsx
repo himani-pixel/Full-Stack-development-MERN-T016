e879b1e5d3cf18dcbdd259ef438d798e492cdc0d
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:35:48 +0530

    Create Practical_1C.jsx

---
+++ Practical 1/Practical_1C.jsx
@@ -0,0 +1,13 @@
+console.log("Basic Calculator...")
+
+
+//Assigning Values
+let a = 11;
+let b = 11;
+
+
+console.log("Addition: " , a+b)
+console.log("Substraction: " , a-b)
+console.log("Multiplication: ",a*b)
+console.log("Divison: ",a/b)
+console.log("Modulas: ",a%b)
