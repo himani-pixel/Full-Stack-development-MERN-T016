e57d20102eebef7b54d61a8b596dac6a46a92c77
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:08:06 +0530

    Create ProfileCard.jsx

---
+++ Practical 3/Practical_3D/ProfileCard.jsx
@@ -0,0 +1,13 @@
+function ProfileCard() {
+  return (
+    <div>
+      <h2>Student Profile</h2>
+      <p>Name: Himani Malankar</p>
+      <p>Course: BSc.IT</p>
+      <p>RollNO: T016</p>
+      <p>College: Sheth L.U.J.and Sir M.V College of Art Commerce and Science.</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
