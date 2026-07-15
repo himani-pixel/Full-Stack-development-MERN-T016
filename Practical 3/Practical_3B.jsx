f61494c1be1f0c714d335e8c7c5e52d846dd6bbe
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:01:33 +0530

    Create Practical_3B.jsx

---
+++ Practical 3/Practical_3B.jsx
@@ -0,0 +1,19 @@
+function App()
+{
+  const emp = "Himani";
+  const dept = "IT";
+  const salary = 100000;
+
+
+  return (
+    <div>
+      <h1>Student Details</h1>
+      <p>Name: {emp}</p>
+      <p>Department: {dept}</p>
+      <p>Salary: ₹{salary}</p>
+    </div>
+  );
+}
+
+
+export default App
