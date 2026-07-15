a5839bdffa658d187cb9c9a53dda5ab6817a2958
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 18:11:14 +0530

    Delete Practical 4/Practical_4A directory

--- Practical 4/Practical_4A/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2026 M. V. L. U. Placement Cell</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- Practical 4/Practical_4A/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(){
-    return(
-        <header className = "header">
-            <h1>Sheth L.U.J and Sir M.V College Placement Dashboard</h1>
-        </header>
-    );
-}
-
-export default Header;
