9a1e86c126cefbd6eb30d06b7fc75a80ecd39c87
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:40:35 +0530

    Create Practical_2C.jsx

---
+++ Practical 2/Practical_2C.jsx
@@ -0,0 +1,23 @@
+//Arrow symbol
+let add = (a , b) =>{
+   return a + b;
+};
+
+
+let sub = (a , b) => {
+   return a - b;
+};
+let multiply = (a , b) => {
+   return a * b;
+};
+
+
+let div = (a , b) => {
+   return a / b;
+}
+
+
+console.log("Addition: ",add(10 , 20));
+console.log("Subtraction: ",sub(20 , 10));
+console.log("Multiplication: ",multiply(5 , 6));
+console.log("Division: ",div(10 , 2));
