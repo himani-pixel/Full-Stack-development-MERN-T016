a5e2e185b7bae3b27abe3b730fe04d130afb7b8b
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:39:58 +0530

    Create Practical_2B.jsx
    Added a new JSX file with arrays and objects examples.

---
+++ Practical 2/Practical_2B.jsx
@@ -0,0 +1,17 @@
+//array
+let fruits = ['Mango' , 'Lichi' , 'Dragonfruit' , 'Watermelon' , 'Mangosteem'];
+
+
+//objects
+let student = {
+   name : 'Himani Malankar',
+   age : 20,
+   city : "Mumbai"
+};
+console.log("Fruits: ",fruits[2]);
+console.log("4th fruit: ",fruits[4]);
+
+
+console.log("Student Name: ",student.name);
+console.log("Student Age: ",student.age);
+console.log("City where student live: ",student.city);
