d0ae330bd53a206c305817b684482e1a761b2c98
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:08:58 +0530

    Create SkillsList.jsx

---
+++ Practical 3/Practical_3D/SkillsList.jsx
@@ -0,0 +1,15 @@
+function SkillsList() {
+  return (
+    <div>
+      <h2>My Skills</h2>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>React</p>
+        <p>Python</p>
+        <p>Data Science</p>
+    </div>
+  );
+}
+
+export default SkillsList;
