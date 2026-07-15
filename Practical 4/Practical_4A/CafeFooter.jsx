5333d5f012bcc459f2494b17ae461014797d9e9f
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 17:04:36 +0530

    Add files via upload

---
+++ Practical 4/Practical_4A/CafeFooter.jsx
@@ -0,0 +1,9 @@
+function CafeFooter() {
+  return (
+    <footer className="footer">
+      <p>Open daily from 9 AM to 9 PM</p>
+    </footer>
+  );
+}
+
+export default CafeFooter;
---
+++ Practical 4/Practical_4A/CafeHeader.jsx
@@ -0,0 +1,11 @@
+function CafeHeader() {
+  return (
+    <header className="header">
+      <h1>Sunrise Cafe</h1>
+      <br></br>
+      <p>Fresh coffee, snacks, and happy vibes.</p>
+    </header>
+  );
+}
+
+export default CafeHeader;
