1bc8804be8d6964c26dff868daa22594102627b7
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:07:36 +0530

    Create WelcomeStudent.jsx

---
+++ Practical 3/Practical_3D/WelcomeStudent.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h2>Welcome to React</h2>
+      <p>React lets us build pages using small reusable components.</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
