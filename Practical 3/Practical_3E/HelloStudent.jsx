160a98ef6090667bf24a0b8dcc155286c54d4088
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 13:13:40 +0530

    Create HelloStudent.jsx

---
+++ Practical 3/Practical_3E/HelloStudent.jsx
@@ -0,0 +1,10 @@
+function HelloStudent() {
+  return (
+    <div>
+      <h2>Hello, student!</h2>
+      <p>This is my first functional component.</p>
+    </div>
+  );
+}
+
+export default HelloStudent;
