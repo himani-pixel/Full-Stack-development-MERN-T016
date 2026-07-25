cb9652bb707ef11be8440bc92bbe7679629474b0
Himani Malankar <himanimalankar@gmail.com>
2026-07-25 14:34:07 +0530

    Update App.jsx

--- Practical 4/Practical_4F/App.jsx
+++ Practical 4/Practical_4F/App.jsx
@@ -5,6 +5,25 @@ function App() {
   // State Variable
   const [count, setCount] = useState(0);
 
+  // Increase Function
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  // Decrease Function
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  // Reset Function
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
       <h1>Button Click Counter</h1>
@@ -12,21 +31,15 @@ function App() {
 
       <h2>Himani Clicks: {count} Times</h2>
 
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
+      <button onClick={increase}>Increase</button>
 
       &nbsp;&nbsp;
 
-      <button onClick={() => setCount(count - 1)}>
-        Decrease
-      </button>
+      <button onClick={decrease}>Decrease</button>
 
       &nbsp;&nbsp;
 
-      <button onClick={() => setCount(0)}>
-        Reset
-      </button>
+      <button onClick={reset}>Reset</button>
 
     </div>
   );
